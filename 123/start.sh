480dbcea74bda3f4b3a307387da783a5538fd67c
baoge456 <gatzpipikq@hotmail.com>
2025-01-01 13:39:47 +0800

    2025-01-01 13:39:48 del 123/start.sh

--- 123/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# 启动 webssh 程序
-nohup python /usr/home/fanuccisr/domains/webssh.baoge456.us.kg/public_html/run.py --port=31400 --font='Consolas.ttf' --xsrf=False --xheaders=False --origin='*' --debug --delay=6 > /dev/null 2>&1 &
